f9bf873031972323f24788aeec5889bc2fb08dad
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-05-01 12:31:32 -0300

    Create task list

---
+++ src/components/Tarefas/Tarefa.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <v-list-item
+      :class="{ 'light-blue lighten-5': tarefa.concluido }"
+      @click="$emit('tarefaConcluida', !tarefa.concluido)"
+    >
+      <template v-slot:default="{}">
+        <v-list-item-action>
+          <v-checkbox 
+            :input-value="tarefa.concluido"
+            ></v-checkbox>
+        </v-list-item-action>
+
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{'text-decoration-line-through': tarefa.concluido}">
+            {{ tarefa.titulo }}
+          </v-list-item-title>
+        </v-list-item-content>
+      </template>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tarefa: Object,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,15 +1,44 @@
 <template>
-  <hello-world />
+  <div>
+    <v-col cols="12">
+      <v-text-field
+        v-model="inputTarefa"
+        label="Qual sua tarefa?"
+        outlined
+        clearable
+        @keydown.enter="adicionarTarefa"
+      ></v-text-field>
+    </v-col>
+    <v-list flat subheader>
+      <v-list-item-group multiple active-class="">
+        <div v-for="(tarefa, index) in tarefas" :key="index">
+          <Tarefa :tarefa="tarefa" @tarefaConcluida="tarefa.concluido = $event" />
+        </div>
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+import Tarefa from "@/components/Tarefas/Tarefa.vue";
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
+export default {
+  name: "Home",
+  components: { Tarefa },
+  data() {
+    return {
+      inputTarefa: null,
+      tarefas: [],
+    };
+  },
+  methods: {
+    adicionarTarefa() {
+      if (this.inputTarefa) {
+        const tarefa = { titulo: this.inputTarefa, concluido: false }
+        this.tarefas.push(tarefa)
+        this.inputTarefa = null
+      }
+    }
   }
+};
 </script>
